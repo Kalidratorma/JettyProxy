ea98de87c3687395bb453da9185649f624610cfd
kalidratorma <kalidratorma@gmail.com>
2023-04-06 18:29:03 +0300

    changed

--- src/main/java/com/kalidratorma/CustomProxyServlet.java
+++ src/main/java/com/kalidratorma/CustomProxyServlet.java
@@ -1,41 +1,65 @@
 package com.kalidratorma;
 
-import org.eclipse.jetty.client.http.HttpClientTransportOverHTTP;
-import org.eclipse.jetty.io.ClientConnector;
-import org.eclipse.jetty.proxy.ProxyServlet;
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.http.HttpClientTransportOverHTTP;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.proxy.ProxyServlet;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Custom implementation of ProxyServlet that allows proxying to specific hosts with customizable TLS parameters.
+ */
 public class CustomProxyServlet extends ProxyServlet {
 
-    private static final Set<String> httpsHosts;
+    /**
+     * Set of host names that should be proxied with custom TLS parameters.
+     */
+    private static final Set<String> HTTPS_HOSTS;
 
     static {
-        httpsHosts = new HashSet<>();
-        httpsHosts.add("twitter.com");
-        httpsHosts.add("skvazy.com");
-        httpsHosts.add("snowmoscow.ru");
+        HTTPS_HOSTS = new HashSet<>();
+        HTTPS_HOSTS.add("twitter.com");
+        HTTPS_HOSTS.add("skvazy.com");
+        HTTPS_HOSTS.add("snowmoscow.ru");
     }
 
+    /**
+     * Creates an instance of {@link HttpClient} with custom TLS parameters.
+     *
+     * @return The created {@link HttpClient} instance.
+     * @throws ServletException If an error occurs while creating the client.
+     */
     @Override
     protected HttpClient createHttpClient() throws ServletException {
-        SslContextFactory sslContextFactory = new SslContextFactory.Server();
-        HttpClient httpClient = new HttpClient(new HttpClientTransportOverHTTP(new ClientConnector()));
+        // Create an instance of SslContextFactory with custom TLS parameters.
+        SslContextFactory sslContextFactory = new SslContextFactory.Client();
+        sslContextFactory.setIncludeProtocols("TLSv1.2", "TLSv1.3");
+        sslContextFactory.setExcludeCipherSuites("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
+
+        // Create an instance of HttpClient with the SslContextFactory.
+        HttpClient httpClient = new HttpClient(new HttpClientTransportOverHTTP());
         try {
             httpClient.start();
-            sslContextFactory.setEndpointIdentificationAlgorithm(null);
         } catch (Exception e) {
             throw new ServletException("Failed to start HttpClient", e);
         }
         return httpClient;
     }
 
+    /**
+     * Rewrites the target URL of the incoming request to use HTTPS if the host is in the set of HTTPS hosts.
+     *
+     * @param request The incoming request.
+     * @return The rewritten URL.
+     */
     @Override
     protected String rewriteTarget(HttpServletRequest request) {
         String rewrittenUrl = super.rewriteTarget(request);
@@ -44,7 +68,7 @@ public class CustomProxyServlet extends ProxyServlet {
         }
 
         URI uri = URI.create(rewrittenUrl);
-        if (httpsHosts.contains(uri.getHost())) {
+        if (HTTPS_HOSTS.contains(uri.getHost())) {
             String httpsUrl = rewrittenUrl.replaceFirst("^http:", "https:");
             return httpsUrl;
         } else {
@@ -52,4 +76,29 @@ public class CustomProxyServlet extends ProxyServlet {
         }
     }
 
+    /**
+     * Forwards the incoming request to the target server, and logs the session key and website for HTTPS requests.
+     *
+     * @param request  The incoming request.
+     * @param response The outgoing response.
+     * @throws ServletException If an error occurs while forwarding the request.
+     * @throws IOException      If an I/O error occurs while forwarding the request.
+     */
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Call the super service method to perform the actual proxying.
+        super.service(request, response);
+
+        // If this was an HTTPS request and the host is in the set of HTTPS hosts, log the session key and website.
+        URI uri = URI.create(request.getRequestURL().toString());
+        if (request.isSecure() && HTTPS_HOSTS.contains(uri.getHost())) {
+// Log the session key.
+            String sessionKey = response.getHeader(HttpHeader.SET_COOKIE.asString());
+            if (sessionKey != null) {
+                System.out.println("Session key: " + sessionKey);
+            }
+            // Log the website.
+            System.out.println("Website: " + uri.getHost());
+        }
+    }
 }
