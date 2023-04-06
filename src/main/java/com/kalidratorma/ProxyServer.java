b3f822bd4768475186b96fd6ec9e5ecebb7e3aaa
kalidratorma <kalidratorma@gmail.com>
2023-04-06 20:46:47 +0300

    changed

--- src/main/java/com/kalidratorma/ProxyServer.java
+++ src/main/java/com/kalidratorma/ProxyServer.java
@@ -1,38 +1,33 @@
 package com.kalidratorma;
 
+import org.eclipse.jetty.proxy.ConnectHandler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.server.handler.HandlerList;
 
 public class ProxyServer {
     public static void main(String[] args) throws Exception {
 
         // Create a new Jetty server instance
-        Server server = new Server();
+        Server server = new Server(8080);
 
-        // Create a new server connector for the Jetty server instance
-        ServerConnector connector = new ServerConnector(server);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
 
-        // Set the port of the server connector to 8080
-        connector.setPort(8080);
-
-        // Add the server connector to the Jetty server instance
-        server.addConnector(connector);
-
-        // Create a new servlet handler for the Jetty server instance
-        ServletHandler servletHandler = new ServletHandler();
-
-        // Set the servlet handler for the Jetty server instance
-        server.setHandler(servletHandler);
-
-        // Create a new instance of the custom proxy servlet
         CustomProxyServlet customProxyServlet = new CustomProxyServlet();
+        ServletHolder proxyServletHolder = new ServletHolder(customProxyServlet);
+        context.addServlet(proxyServletHolder, "/*");
 
-        // Add the custom proxy servlet to the servlet handler with a URL mapping of "/*"
-        servletHandler.addServletWithMapping(new ServletHolder(customProxyServlet), "/*");
+        // Add ConnectHandler to support HTTPS
+        ConnectHandler proxy = new ConnectHandler();
+        proxy.setServer(server);
 
-        // Start the Jetty server instance
+        HandlerList handlers = new HandlerList();
+        handlers.addHandler(proxy);
+        handlers.addHandler(context);
+
+        server.setHandler(handlers);
         server.start();
 
         // Wait for the Jetty server instance to finish execution
